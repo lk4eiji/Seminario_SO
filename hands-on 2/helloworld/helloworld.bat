655a042b13f074b134c294b8b2e8165de5ba59bb
Abraham Mendoza <39322065+lk4eiji@users.noreply.github.com>
2024-09-28 17:41:41 -0600

    Create helloworld.bat

---
+++ hands-on 2/helloworld/helloworld.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Imprimir en pantalla "Hello World"
+echo Hello World
+
+echo Listando el contenido del directorio actual:
+dir
+
+:: Crear un subdirectorio Test
+mkdir Test
+
+:: Cambiar al subdirectorio Test
+cd Test
+
+:: Mostrar el contenido del subdirectorio Test
+echo Listando el contenido del subdirectorio Test:
+dir
