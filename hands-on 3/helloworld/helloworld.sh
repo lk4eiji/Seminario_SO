0bb345a235086c8223e7f00d8c9e45c644174c3d
Abraham Mendoza <39322065+lk4eiji@users.noreply.github.com>
2024-09-28 18:40:56 -0600

    Create helloworld.sh

---
+++ hands-on 3/helloworld/helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Imprimir en pantalla "Hello World"
+echo "Hello World"
+
+# Mostrar contenido del directorio actual
+echo "Mostrando el contenido del directorio actual:"
+ls
+
+# Crear directorio Test
+mkdir Test
+
+# Moverse al directorio Test
+cd Test
+
+# Mostrar contenido del directorio Test
+echo "Mostrando el contenido del directorio Test:"
+ls
